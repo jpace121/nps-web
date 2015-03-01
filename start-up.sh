7294f1e2fc6809526b78b8c14bff7552abde14a2
James Pace <jwp12c@my.fsu.edu>
2015-03-01 22:30:45 +0000

    Wifi works.

--- start-up.sh
+++ start-up.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo '-------------------------'
+
 # This scipt shoulf be ran everytime at start up.
 # The script handles the stuff that must be done everytime at boot.
 echo '==> Bind Bluetooth port.'
@@ -8,11 +10,10 @@ echo '==> Set up device tree overlay.'
 echo BB-SPIDEV0 >>  /sys/devices/bone_capemgr.9/slots
 
 echo '==> Wifi set up.'
-ifdown wlan0
+modprobe  8192cu
 ifup wlan0
-service dnsmasq restart
+service dnsmasq restart 
 service hostapd restart
-#hostapd -B /root/python-bluetooth/utils/hostapd.conf
 
 echo '==> Start server.'
 python /root/python-bluetooth/serve_tornado.py &
