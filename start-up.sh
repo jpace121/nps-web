454360a5f809f8f8a31c036b0ee427da68343df6
James Pace <jwp12c@my.fsu.edu>
2015-03-01 23:04:23 +0000

    Works from start-up script.
    DNS redirect does not work.
    Only tested once total.

--- start-up.sh
+++ start-up.sh
@@ -1,9 +1,16 @@
-# to be ran everytime at start up.
+# This scipt shoulf be ran everytime at start up.
+# The script handles the stuff that must be done everytime at boot.
+echo '==> Bind Bluetooth port.'
 rfcomm bind /dev/rfcomm0 00:13:43:02:34:2E
+echo '==> Set up device tree overlay.'
 echo BB-SPIDEV0 >>  /sys/devices/bone_capemgr.9/slots
 
+echo '==> Wifi set up.'
 ifup wlan0
-hostapd -B /root/python-bluetooth/utils/hostapd.conf
+service dnsmasq restart
+service hostapd restart
+#hostapd -B /root/python-bluetooth/utils/hostapd.conf
 
+echo '==> Start server.'
 python /root/python-bluetooth/serve_tornado.py
 
