16dff27129a7c6cf2b3c73afea58f1ae57f0de46
James Pace <jwp12c@my.fsu.edu>
2015-03-02 02:10:08 +0000

    enable rediculous logging

--- serve_tornado.py
+++ serve_tornado.py
@@ -3,6 +3,9 @@ from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from website import app
 
+from tornado.log import enable_pretty_logging
+enable_pretty_logging()
+
 http_server = HTTPServer(WSGIContainer(app))
 print "Serving on port 80"
 http_server.listen(80)
