45e163cea83789d073ee70f49644d3c00c6c00c3
Jimmy Pace <pacejimmy@yahoo.com>
2015-06-07 23:52:05 -0400

    Reading from Arduino works.

---
+++ static/js/range_finder.js
@@ -0,0 +1,41 @@
+/*
+  Javascript for range_finder.html
+  This should be refactored to something that uses less
+  anonymous functions.
+*/
+
+//Start Streaming
+$(function () {
+    $('#start-stream-btn').bind('click',function () {
+        $.getJSON($SCRIPT_ROOT + '/_get_range_vals',{
+            option: "stream_start"
+       }, function(data) {
+           $("#result").text(data.result)
+       });
+        return false; //to remove the button from hreffing
+        })
+});
+
+//Stop Streaming
+$(function () {
+    $('#stop-stream-btn').bind('click',function () {
+        $.getJSON($SCRIPT_ROOT + '/_get_range_vals',{
+            option: "stream_stop"
+       }, function(data) {
+           $("#result").text(data.result)
+       });
+        return false; //to remove the button from hreffing
+        })
+});
+
+//Read Once
+$(function () {
+    $('#once-btn').bind('click',function () {
+        $.getJSON($SCRIPT_ROOT + '/_get_range_vals',{
+            option: "once"
+       }, function(data) {
+           $("#result").text(data.result)
+       });
+        return false; //to remove the button from hreffing
+        })
+});
--- website.py
+++ website.py
@@ -1,20 +1,58 @@
 from distanceSensor import DistanceSensor
 from flask import Flask, request, session, g, redirect, url_for, \
-    abort, render_template, flash
+    abort, render_template, flash, jsonify
+from time import sleep
 
-app = Flask(__name__) #I'm globla. Is this ok?
+app = Flask(__name__)
 app.config['DEBUG'] = True # should be False in production
 
+# Sensor related gloabl variables
+range_finder = DistanceSensor('/dev/cu.usbmodem1421')
+
 @app.route('/')
-def index():
-    return render_template('index.html',user="Jimmy")
+@app.route('/index.html')
+def index_html():
+    return render_template('index.html')
 
 @app.route('/receive', methods=['POST','GET'])
 def receive():
+    pass;
+"""
     number1 = request.form['number1']
     number2 =request.form['number2']
     summed = int(number1) + int(number2)
     return render_template('receive.html',sum=summed)
+"""
+@app.route('/range_finder.html')
+def range_finder_html():
+    global range_finder
+    range_finder.connect()
+    return render_template('range_finder.html')
+
+@app.route('/_get_range_vals') #I may want to make this a post instead of get?
+def get_range_values_get():
+    global range_finder
+    option = request.args.get('option','None')
+    if option == "stream_start":
+        while True:
+            if range_finder.isAlive():
+                range_finder.streamStart()
+                response = "stream_started"
+                break
+            else:
+                sleep(0.25)
+    elif option == "stream_stop":
+        response = range_finder.streamStop()
+    elif option == "once":
+        while True:
+            if range_finder.isAlive():
+                response = range_finder.getDistance()
+                break
+            else:
+                sleep(0.25)
+    else:
+        response = "error"
+    return jsonify(result=response)
 
 if __name__ == '__main__':
     app.run()
