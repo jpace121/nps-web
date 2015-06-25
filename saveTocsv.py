817e398189023998cabcddcb72cdcef1abd0c6d9
Jimmy Pace <pacejimmy@yahoo.com>
2015-06-25 10:34:41 -0400

    savetocsv makes a valid csv, not sure if columns are correct.

--- saveTocsv.py
+++ saveTocsv.py
@@ -13,7 +13,9 @@ which can be read in excel.
 def makeDataFileName():
     # inspired by http://stackoverflow.com/questions/13890935/timestamp-python
    ts = time.time()
-   return datetime.datetime.fromtimestamp(ts).strftime('penetrometer_%Y%m%d_%H%M%S.csv')
+   filename = datetime.datetime.fromtimestamp(ts).strftime('penetrometer_%Y%m%d_%H%M%S.csv')
+   filepath = "./logs/"
+   return filepath+filename
 
 def jsonToCSV(json_file):
    # this is nonoptimal and prbably overly slow...
@@ -24,9 +26,26 @@ def jsonToCSV(json_file):
       for second_key in json_file[first_key].keys():
          big_dict[first_key+"_"+second_key] = json_file[first_key][second_key]
          
-   # Step 2: Write header
-   
-   np.savetxt(makeDataFileName(),big_dict)
+   # Step 2: What is the max number of iterations needed to loop through the longest list?
+   max_len = 0
+   for key in big_dict.keys():
+      if len(big_dict[key]) > max_len:
+         max_len = len(big_dict[key])
+
+   with open(makeDataFileName(),'w') as csv_file:
+      writer = csv.writer(csv_file)
+      # Step 3: Write first row.
+      writer.writerow(big_dict.keys())
+      # Step 4: Loop through that number of times. If the value is missing, put a " ".
+      for i in range(0,max_len):
+         row = []
+         for key in big_dict.keys():
+            try:
+               elem = big_dict[key][i]
+            except IndexError:
+               elem = " "
+            row.append(elem)
+         writer.writerow(row)
 
 if __name__ == "__main__":
     with open('./test.json','r') as f:
